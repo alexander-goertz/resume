bbeadfa050ff60575986042a2eae206eab49f776
Alex <goertz@tuta.io>
2024-02-18 14:07:04 +0100

    removed manual aws config update

--- server/db.mjs
+++ server/db.mjs
@@ -1,22 +1,15 @@
-import AWS from 'aws-sdk';
+import AWS from "aws-sdk";
 import "dotenv/config.js";
-
-AWS.config.update({
-    region: "eu-central-1",
-    accesKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-})
 
 const ddbClient = new AWS.DynamoDB.DocumentClient();
 
-
 const getWorkExperience = async () => {
-    const params = {
-        TableName: "work_experience"
-    }
+  const params = {
+    TableName: "work_experience",
+  };
 
-    const workExperience = await ddbClient.scan(params).promise()
-    return workExperience
-}
+  const workExperience = await ddbClient.scan(params).promise();
+  return workExperience;
+};
 
-export { getWorkExperience }
+export { getWorkExperience };
